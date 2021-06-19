5574605df1468833b392a1f7d9692226006c1dcf
MeAtharva <meatharva24@gmail.com>
2021-06-19 17:27:27 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,90 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Atharva Arjun",
+    "role": "Web Developer and Competitive Programmer",
+    "linkedinId":"https://www.linkedin.com/in/atharva-a-9a62b21a3/",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/atharva-a-9a62b21a3/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/MeAtharva/MeAtharva",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/AtharvaArjun",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a third year student at Shri Guru Gobind Singhji Institute of Engineering and Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer and a Competitive Programmer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"8379070734",
+    "education":[
+      {
+        "UniversityName":"Shri Guru Gobind Singhji Institute of Engineering and Technology",
+        "specialization":"Computer Science and Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2023",
+      },
+      {
+        "UniversityName":"Shri Tripura Junior Science College",
+        "specialization":"12th",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2019",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Freelancer",
+        "specialization":"Graphic Designer, Front-End Web Developer",
+        "MonthofPassing":"Dec",
+      }
+    ],
+    "skillsDescription":"Here are my Skills!",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Django"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"File-manager in Python",
+        "description":"Python Program",
+        "imgurl":"images/file-manager.png"
+      },
+      {
+        "name":"Dodge Car Game",
+        "description":"Pygame Program",
+        "imgurl":"images/Dodge Game.jpg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"Leadership Needs Compassion To Thrive",
+        "name":"Yohancé Salimu"
+      },
+      {
+        "description":"That which does not kill us makes us stronger.",
+        "name":"Friedrich Nietzsche"
+      }
+    ]
+  }
+  
+  export default resumeData
