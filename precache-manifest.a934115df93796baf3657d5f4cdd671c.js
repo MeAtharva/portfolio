d5d7d8d16272e49814f91fbc319b1b3b43be1b21
MeAtharva <meatharva24@gmail.com>
2021-06-19 17:40:17 +0530

    Updates

---
+++ precache-manifest.a934115df93796baf3657d5f4cdd671c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a41ba8bdc3eb10777ca",
+    "url": "/portfolio/static/css/main.7a913443.chunk.css"
+  },
+  {
+    "revision": "0a41ba8bdc3eb10777ca",
+    "url": "/portfolio/static/js/main.0a41ba8b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "158af74c24d4b47818f87bdb542fc4c0",
+    "url": "/portfolio/index.html"
+  }
+];
